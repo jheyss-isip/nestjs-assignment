03e114a46617553c518aa54d8806eae32733c039
jheyss-isip <jheysielalongisip@gmail.com>
2024-11-10 16:12:34 +0800

    Completed Assignment 3: Factorial Calculator

--- assignments/assignments.controller.ts
+++ assignments/assignments.controller.ts
@@ -36,4 +36,23 @@ export class AssignmentsController {
 
     return true;
   }
+
+  @Get('factorial/:number')
+  calculateFactorial(@Param('number') number: number): { factorial: number } {
+    const factorial = this.getFactorial(Number(number));
+    return { factorial };
+  }
+
+  private getFactorial(number: number): number {
+    if (number < 0) {
+      throw new Error('Factorial is not defined for negative numbers');
+    }
+    if (number === 0 || number === 1) return 1;
+
+    let result = 1;
+    for (let i = 2; i <= number; i++) {
+      result *= i;
+    }
+    return result;
+  }
 }
