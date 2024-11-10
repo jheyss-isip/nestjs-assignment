20ea7003c2cde16605335a77e6c335d060122b98
jheyss-isip <jheysielalongisip@gmail.com>
2024-11-10 16:11:20 +0800

    Completed Assignment 1: Fibonacci Sequence

--- assignments/assignments.controller.ts
+++ assignments/assignments.controller.ts
@@ -18,4 +18,22 @@ export class AssignmentsController {
     }
     return sequence;
   }
+
+  @Get('prime/:number')
+  checkPrime(@Param('number') number: number): { isPrime: boolean } {
+    const isPrime = this.isPrime(Number(number));
+    return { isPrime };
+  }
+
+  private isPrime(number: number): boolean {
+    if (number <= 1) return false;
+    if (number <= 3) return true;
+    if (number % 2 === 0 || number % 3 === 0) return false;
+
+    for (let i = 5; i * i <= number; i += 6) {
+      if (number % i === 0 || number % (i + 2) === 0) return false;
+    }
+
+    return true;
+  }
 }
